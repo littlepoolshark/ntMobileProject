f663c7b5680d9ffb92efbe9a74f0dad0f2a99821
sam liu <1145694956@qq.com>
2016-07-01 18:18:18 +0800

    提交漏掉的。

---
+++ src/js/pages/utilities/FundGuaranteeDescription.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Group from "../../UIComponents/Group";
+
+// 资金保障组件
+//这是无状态组件，意在整个组件树更新的时候，跳过virtual DOM的diff比较，从而提高性能。
+//为了避免一定程度的浪费，react官方还在0.14版本中加入了无状态组件
+function FundGuaranteeDescription(props){
+    return (
+        <Group>
+            <h6>资金保障</h6>
+            <div className="content">
+                一、用户资金存放以及流转完全通过第三方存管系统，农泰金融不触碰用户交易资金。<br/>
+                二、用户资金同卡进出，到期本息返还至用户账户。<br/>
+                三、阳光保险对农泰金融账户被盗转，盗用导致的直接经济损失，保险理赔。<br/>
+            </div>
+        </Group>
+    )
+}
+
+export  default FundGuaranteeDescription;
